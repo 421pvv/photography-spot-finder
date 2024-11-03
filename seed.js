bb66c27df8dfe3fe06fb2146b2abf3ac1879541a
sivaanand13 <SIVAANAND.IN@GMAIL.COM>
2024-11-02 22:47:48 -0400

    Modifying seed file to avoid mongo name space error

--- seed.js
+++ seed.js
@@ -2,5 +2,5 @@ import * as mongoCollections from "./config/mongoCollections.js";
 
 export async function initDB() {
   const usersCollection = await mongoCollections.users();
-  await usersCollection.drop();
+  await usersCollection.deleteMany({});
 }
