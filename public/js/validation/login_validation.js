a39d66539c5acd894000f8dfa419f0dec31966ff
421pvv <421pvv@gmail.com>
2024-11-10 22:51:26 -0500

    Added Client Side scripts and modified template for user login

---
+++ public/js/validation/login_validation.js
@@ -0,0 +1,138 @@
+let loginForm = document.getElementById("login-form");
+let usernameInput = document.getElementById("user-name");
+let passwordInput = document.getElementById("password");
+let resetButton = document.getElementById("clear-login-form");
+let errorsDiv = document.getElementById("form-errors-div");
+
+// checking the inputs in login form
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  errorsDiv.innerHTML = "";
+  let errors = [];
+  try {
+    usernameInput.value = validateUsername(usernameInput.value, "Username");
+  } catch (e) {
+    errors = errors.concat(e);
+  }
+  try {
+    validateLoginPassword(passwordInput.value, "Password");
+  } catch (e) {
+    errors = errors.concat(e);
+  }
+  if (errors.length !== 0) {
+    const errorsList = document.createElement("ul");
+    for (const error of errors) {
+      const listItem = document.createElement("li");
+      listItem.textContent = error;
+      errorsList.appendChild(listItem);
+    }
+    errorsDiv.appendChild(errorsList);
+  } else {
+    loginForm.submit();
+  }
+});
+
+// code to reset the form
+if (resetButton) {
+  resetButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    errorsDiv.innerHTML = "";
+    usernameInput.value = "";
+    passwordInput.value = "";
+  });
+}
+
+// Validation code
+const usernamePolicies = [
+  { regex: /^[^ ]{6,}$/, error: "Username must have at least six characters!" },
+];
+validateString = (str, varName) => {
+  if (str == undefined) {
+    throw [
+      `Expected ${
+        varName ? varName : ""
+      } to be of type string, but it is not provided!`,
+    ];
+  }
+  if (str == null) {
+    throw [
+      `Expected ${
+        varName ? varName : ""
+      } to be of type string, but it is null!`,
+    ];
+  }
+  if (Array.isArray(str)) {
+    throw [
+      `Expected ${
+        varName ? varName : ""
+      } to be of type string, but it is an array!`,
+    ];
+  }
+  if (typeof str !== "string") {
+    throw [
+      `Expected ${
+        varName ? varName : ""
+      } to be of type string, but it is of type ${typeof str}!`,
+    ];
+  }
+  const trimmedStr = str.trim();
+  if (trimmedStr.length === 0) {
+    throw [`String ${varName ? varName : ""} is empty or has only spaces!`];
+  }
+  return trimmedStr;
+};
+
+function validateUsername(str, varName) {
+  let errors = [];
+  try {
+    str = validateString(str, varName);
+  } catch (e) {
+    errors = errors.concat(e);
+  }
+
+  for (const policy of usernamePolicies) {
+    if (typeof str !== "string" || !policy.regex.test(str)) {
+      errors = errors.concat(policy.error);
+    }
+  }
+
+  if (errors.length !== 0) {
+    throw errors;
+  }
+
+  return str.toLowerCase();
+}
+
+const validateLoginPassword = (str, varName) => {
+  if (str == undefined) {
+    throw [
+      `Expected ${
+        varName ? varName : ""
+      } to be of type string, but it is not provided!`,
+    ];
+  }
+  if (str == null) {
+    throw [
+      `Expected ${
+        varName ? varName : ""
+      } to be of type string, but it is null!`,
+    ];
+  }
+  if (Array.isArray(str)) {
+    throw [
+      `Expected ${
+        varName ? varName : ""
+      } to be of type string, but it is an array!`,
+    ];
+  }
+  if (typeof str !== "string") {
+    throw [
+      `Expected ${
+        varName ? varName : ""
+      } to be of type string, but it is of type ${typeof str}!`,
+    ];
+  }
+  if (str.length === 0) {
+    throw [`String ${varName ? varName : ""} is empty`];
+  }
+};
