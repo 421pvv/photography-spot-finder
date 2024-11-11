52e326c73fc2f8c3459f626398d5331d569489a3
421pvv <421pvv@gmail.com>
2024-11-10 23:43:34 -0500

    Used p tags instead of ul for form errors

--- public/js/validation/login_validation.js
+++ public/js/validation/login_validation.js
@@ -20,13 +20,11 @@ loginForm.addEventListener("submit", (event) => {
     errors = errors.concat(e);
   }
   if (errors.length !== 0) {
-    const errorsList = document.createElement("ul");
     for (const error of errors) {
-      const listItem = document.createElement("li");
-      listItem.textContent = error;
-      errorsList.appendChild(listItem);
+      const pItem = document.createElement("p");
+      pItem.textContent = error;
+      errorsDiv.appendChild(pItem);
     }
-    errorsDiv.appendChild(errorsList);
   } else {
     loginForm.submit();
   }
